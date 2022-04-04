8dfa1d3f61a61b146245b3823c3179c3a81e05b9
barbararibeiroo <barbararibeiro18@gmail.com>
2022-04-04 10:56:08 -0300

    construindo_API_REST

---
+++ 0x02/construindo_API_REST/com/example/demo/SongRepository.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SongRepository {
+   private List<Song> list = new ArrayList<Song>();
+
+   public List<Song> getAllSongs() {
+    return list;
+   }
+
+   public Song getSongById(Integer id) {
+	for (Song song : list) {
+		if(song.getId().equals(id)) {
+			return new Song(song.getId(), song.getNome(), song.getArtista(), song.getAlbum(), song.getAnoLancamento());
+		}
+	}
+	   
+	   return null;
+     
+   }
+
+   public void addSong(Song s) {
+	   list.add(s);
+   }
+
+   public void updateSong(Song s) {
+	   for(Song songs : list) {
+	    	  if(songs.getId().equals(s)) {
+	    		  list.replaceAll(r -> s);;
+	    	  }
+	      }
+   }
+
+   public void removeSong(Song s) {
+      list.remove(s);
+   }
+}
